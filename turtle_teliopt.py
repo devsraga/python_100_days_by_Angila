7b2279c02ad4e3956980d683bfb7af518625cbaa
devpooja <er.devsingh1001@gmail.com>
2023-03-18 00:53:35 +0530

    teliopt control is added

---
+++ turtle_teliopt.py
@@ -0,0 +1,48 @@
+
+from turtle import Turtle, Screen
+import random
+
+
+def main():
+    screen = Screen()
+    screen.listen()
+    screen.onkey(go_straight, "Up")
+    screen.onkey(go_back, "Down")
+    screen.onkey(turn_right, "Right")
+    screen.onkey(turn_left, "Left")
+    screen.onkey(clear, "space")
+    screen.exitonclick()
+
+
+def go_straight():
+
+    timy.fd(30)
+
+
+def go_back():
+    timy.back(30)
+
+
+def turn_left():
+    timy.lt(10)
+
+
+def turn_right():
+    timy.rt(30)
+
+
+def clear():
+    timy.clear()
+    timy.penup()
+    timy.home()
+    timy.pendown()
+
+
+
+
+
+if __name__ == "__main__":
+    timy = Turtle()
+    timy.shape("turtle")
+    timy.color("black")
+    main()
